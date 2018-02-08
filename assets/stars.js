f0b86597dd4930ce7650ac46c3278114f3bc065d
Xavier Brinon <xbrinon@opentext.com>
2018-02-08 12:19:46 +0000

    multiple pages, redirection, data

---
+++ assets/stars.js
@@ -0,0 +1,20 @@
+const chaplin = {
+  'firstName': 'Charles',
+  'lastName': 'Chaplin',
+  'tag': 'The little tramp',
+  'content': '...',
+  'movies': []
+}
+
+const marilyn = {
+  'firstName': 'Marilyn',
+  'lastName': 'Monroe',
+  'tag': 'Being normal is boring',
+  'content': '...',
+  'movies': []
+}
+
+module.exports = {
+  chaplin,
+  marilyn
+}
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+const {chaplin, marilyn} = require('./assets/stars')
 const express = require('express')
 
 const app = express()
 app.set('view engine', 'pug') // pug pages are expected to be in the folder 'views'
 
-const sendHome = (req, res) => {
-  res.render('index') // views/index.pug
-}
+const sendHome = (req, res) => res.render('index') // views/index.pug
+const sendChaplin = (req, res) => res.render('chaplin', chaplin)
+const sendMarilyn = (req, res) => res.render('marilyn', marilyn)
+const sendJean = (req, res) => res.render('jean')
+const redirectChaplin = (req, res) => res.redirect('/chaplin')
+
+app.get('/', sendHome)
+app.get('/chaplin', sendChaplin)
+app.get('/charles', redirectChaplin)
+app.get('/marilyn', sendMarilyn)
+app.get('/jean', sendJean)
 
 const port = process.env.port || 5656
-app.get('/', sendHome)
 app.listen(port, _ => {
   console.log(`Listenning to port ${port}.`)
 })
