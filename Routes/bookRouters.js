942f22d7bbb7aa6d31aaf83f2693434da4f321e8
Xavier Brinon <xbrinon@opentext.com>
2018-02-08 17:19:07 +0000

    REST

--- Routes/bookRouters.js
+++ Routes/bookRouters.js
@@ -1,13 +1,57 @@
-const books = require('../assets/books')
+const Books = require('../models/bookModels')
 
 const express = require('express')
 const bookRouter = express.Router()
 
-const sendBooks = (req, res) => res.json(books)
-const sendBookX = id => (req, res) => res.json(books.books.filter(book => book.id === id))
+const sendBooks = (req, res) => Books.find({}, (_, books) => res.json(books))
+const postBook = (req, res) => {
+  const book = new Books(req.body) // Thanks to the middleware body-parser
+  book.save()
+  res.status(201).send(book)
+}
 
-bookRouter
-  .get('/', sendBooks)
-  .get('/2', sendBookX(2))
+// req.book is provided by the middleware fired before the routing.
+const getBook = (req, res) => res.json(req.book)
+const putBook = (req, res) => {
+  req.book.title = req.body.title
+  req.book.author = req.body.author
+  req.book.save()
+  return res.json(req.book)
+}
+const patchBook = (req, res) => {
+  if (req.body._id) {
+    delete req.body._id
+  }
+  for (let p in req.body) {
+    req.book[p] = req.body[p]
+  }
+  req.book.save()
+  return res.json(req.book)
+}
+const deleteBook = (req, res) => req.book.remove(err => err
+  ? res.status(500).send(err)
+  : res.status(204).send('removed')
+)
+
+bookRouter.route('/')
+  .get(sendBooks)
+  .post(postBook)
+
+bookRouter.use('/:bookId', (req, res, next) => {
+  Books.find(req.param.bookId, (err, book) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      req.book = book
+      next()
+    }
+  })
+})
+
+bookRouter.route('/:bookId')
+  .get(getBook)
+  .put(putBook)
+  .patch(patchBook)
+  .delete(deleteBook)
 
 module.exports = bookRouter
--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express()
 app.set('view engine', 'pug') // pug pages are expected to be in the folder 'views'
 app.use(express.static(`${__dirname}/public`)) // all the files will be stored in '/public'
 
+const mongoose = require('mongoose')
+const user = encodeURIComponent('xbrinon')
+const sayFriend = encodeURIComponent('Qt^jNXwM eo(5Cmnm')
+const db = mongoose.connect(`mongodb://${user}:${sayFriend}@ds129428.mlab.com:29428/xb_books`)
+
+const bodyParser = require('body-parser')
+
 const sendHome = (req, res) => res.render('index') // views/index.pug
 const sendChaplin = (req, res) => res.render('chaplin', chaplin)
 const sendMarilyn = (req, res) => res.render('marilyn', marilyn)
@@ -19,6 +26,8 @@ app.get('/marilyn', sendMarilyn)
 app.get('/jean', sendJean)
 
 app.use('/api/books', bookRouter)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ 'extended': true }))
 
 const port = process.env.port || 5656
 app.listen(port, _ => {
