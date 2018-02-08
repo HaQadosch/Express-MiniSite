7ff8ad14283c2858dafcdcbe935235366df7162f
Xavier Brinon <xbrinon@opentext.com>
2018-02-08 11:41:02 +0000

    pug engine setup

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 
-const port = process.env.port || 5656
-
 const app = express()
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.set('view engine', 'pug') // pug pages are expected to be in the folder 'views'
+
+const sendHome = (req, res) => {
+  res.render('index') // views/index.pug
+}
+
+const port = process.env.port || 5656
+app.get('/', sendHome)
 app.listen(port, _ => {
   console.log(`Listenning to port ${port}.`)
 })
